d2a150542c174bb53058e71b67ddea06ae56ebf1
kmi <sageglorieux@hotmail.com>
2021-07-19 20:20:55 -0700

    seo modified

--- src/assets/seo.ts
+++ src/assets/seo.ts
@@ -6,7 +6,7 @@ export const config = {
   //   summary: 'Web developer/programmer',
   // },
   description:
-    'Catch the latest news, in English/Japanese. Can search articles by keywords. Can register your current search query as your favorite, and call them later (cookie must be enabled).',
+    'Catch the latest news with FreeNews API, in English/Japanese. Can search articles by keywords, topic, sources and date up to a month old. Can register your current search keywords as your favorites, and call them later (cookie must be enabled). Build by React.js, Next.js, Material-UI',
   keywords: 'Latest News, React.js, Next.js, Material-UI, FreeNews',
   social: {
     twitter: '',
