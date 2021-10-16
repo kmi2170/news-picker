418480eb4a56fa7adfcef75284bb49ac54487acf
kmi <sageglorieux@hotmail.com>
2021-10-15 18:46:11 -0700

    set timeout option for axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -185,6 +185,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const options = {
     method: "GET",
     url: "https://free-news.p.rapidapi.com/v1/search",
+    timeout: 4500,
     params,
     headers: {
       "x-rapidapi-key": "e55c60efe5msh73070d6e421d34bp11cc43jsn5f182a073484",
