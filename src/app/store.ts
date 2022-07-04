2a87b9e47ae8f16964389a4ba9be9183aa9ded80
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2022-07-04 12:55:35 -0700

    modify store.ts, add test-utils.tsx and install msw for testing with RTK
    	modified:   src/app/store.ts
    
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/store.ts
    	new file:   src/components/Navbar/Searchbar/index.test.tsx
    	new file:   src/utils/test-utils.tsx

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,22 +1,37 @@
-import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import {
+  Action,
+  combineReducers,
+  configureStore,
+  PreloadedState,
+  ThunkAction,
+} from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 
 import newsSlice from '../features/newsSlice';
 import { newsApi } from '../services/newsApi';
 
-export const store = configureStore({
-  reducer: {
-    news: newsSlice,
-    [newsApi.reducerPath]: newsApi.reducer,
-  },
-  middleware: gDM =>
-    gDM({ serializableCheck: false }).concat(newsApi.middleware),
+const rootReducer = combineReducers({
+  news: newsSlice,
+  [newsApi.reducerPath]: newsApi.reducer,
 });
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+    middleware: (gDM) =>
+      gDM({ serializableCheck: false }).concat(newsApi.middleware),
+  });
+};
+
+export const store = setupStore();
 
 setupListeners(store.dispatch);
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
+
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
---
+++ src/components/Navbar/Searchbar/index.test.tsx
@@ -0,0 +1,55 @@
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+// import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '../../../utils/test-utils';
+import SearchBar from './index';
+
+const handlers = [
+  rest.get('/api/geolocation', (req, res, ctx) => {
+    return res(ctx.json([]), ctx.delay(150));
+  }),
+];
+
+const server = setupServer(...handlers);
+// Enable API mocking before tests.
+beforeAll(() => server.listen());
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers());
+// Disable API mocking after the tests are done.
+afterAll(() => server.close());
+
+const user = userEvent.setup();
+const setup = () => render(<SearchBar />);
+
+describe('SearchLocationBar', () => {
+  const testSearchTerm = 'seattle, wa';
+  it(`type in "${testSearchTerm}", then the value has "${testSearchTerm}"`, async () => {
+    setup();
+    await user.type(getTextbox(), testSearchTerm);
+    expect(getTextbox()).toHaveValue(testSearchTerm);
+  });
+
+  it(`type in "${testSearchTerm}", then click clear icon and the value has "" `, async () => {
+    setup();
+    await user.type(getTextbox(), testSearchTerm);
+    await user.click(getIcon(/clear button/i));
+    expect(getTextbox()).toHaveValue('');
+  });
+
+  it(`type in random text "abc123ABC", then click search icon and displays examples for search terms`, async () => {
+    setup();
+    await user.type(getTextbox(), 'abc123ABC');
+    await user.click(getIcon(/search button/i));
+    expect(await screen.findByText(/no place found/i)).toBeInTheDocument();
+  });
+});
+
+const getTextbox = () => screen.getByRole('textbox');
+
+const getIcon = (icon_name: RegExp) =>
+  screen.getByRole('img', {
+    name: icon_name,
+  });
