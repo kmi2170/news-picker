94510c3efb589a0609b0bdd1180cda8bb84a0609
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-15 19:45:38 -0800

    refactor footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,31 +1,18 @@
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import { memo } from 'react';
+import Typography from "@mui/material/Typography";
 
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer>
-      <Grid container>
-        <Grid item xs={12}>
-          <Typography variant="body2">
-            Copyrihgt &copy; kmi {year}. All rights reserved. | Powerd by{' '}
-            {
-              <a
-                href="https://newscatcherapi.com/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                NewsCatcher
-              </a>
-            }
-            .
-          </Typography>
-        </Grid>
-      </Grid>
-    </footer>
+    <Typography component="footer" variant="body2">
+      &copy; {year} Kemmei H.| Powered by{" "}
+      {
+        <a href="https://newsapi.org/" rel="noreferrer" target="_blank">
+          News API
+        </a>
+      }
+    </Typography>
   );
 };
 
-export default memo(Footer);
+export default Footer;
