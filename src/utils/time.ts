9741a2082f57fd47d443abfccdeb6d3f55f3ecb8
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-17 01:51:54 -0800

    fix time from now

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,12 +1,12 @@
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import dayjs from "dayjs";
+// import utc from "dayjs/plugin/utc";
+import relativeTime from "dayjs/plugin/relativeTime";
+import timezone from "dayjs/plugin/timezone";
 
-dayjs.extend(utc);
+// dayjs.extend(utc);
+dayjs.extend(timezone);
 dayjs.extend(relativeTime);
 
 export const timeFromNow = (time: string) => {
-  return dayjs(time)
-    .utc(true)
-    .fromNow();
+  return dayjs(time).fromNow();
 };
