3d6f5042be94506327d9ebbea19ef58fc20e78f5
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-17 13:20:08 -0800

    add convertToLocalTime

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,12 +1,16 @@
 import dayjs from "dayjs";
-// import utc from "dayjs/plugin/utc";
+import utc from "dayjs/plugin/utc";
 import relativeTime from "dayjs/plugin/relativeTime";
 import timezone from "dayjs/plugin/timezone";
 
-// dayjs.extend(utc);
+dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(relativeTime);
 
-export const timeFromNow = (time: string) => {
-  return dayjs(time).fromNow();
+export const timePeriodFromNow = (time: string) => {
+  return dayjs.utc(time).fromNow();
+};
+
+export const convertToLocalTime = (time: string) => {
+  return dayjs(time).local().format(" h:mma MMM DD");
 };
