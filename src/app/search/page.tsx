fa1445a3348fa760c3abfa4019c659444a59c336
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-22 20:02:20 -0800

    modify type of searchParams according to the official document

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -5,19 +5,15 @@ import Typography from "@mui/material/Typography";
 
 import News from "../../components/news";
 import SearchNews from "../../components/search/search-bar";
+import { Language } from "../../api/types";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: {
-    q?: Promise<string>;
-    language?: Promise<string>;
-    page?: Promise<string>;
-  };
-}) {
-  const q = ((await searchParams)?.q || "") as string;
-  const language = ((await searchParams)?.language || "en") as string;
-  const page = Number((await searchParams)?.page) || 1;
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+export default async function Page(props: { searchParams: SearchParams }) {
+  const searchParams = await props.searchParams;
+  const q = (searchParams?.q || "") as string;
+  const language = (searchParams?.language || "en") as Language;
+  const page = Number(searchParams?.page) || 1;
 
   return (
     <Container maxWidth="xl">
