ee6ed00b44812839935864f26e2007e75560de53
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-24 00:52:10 -0800

    inculde dummy data

---
+++ src/dummyNewsData/dummy-evertything.ts
@@ -0,0 +1,260 @@
+export const dummyEverything = {
+  status: "ok",
+  totalResults: 12455,
+  articles: [
+    {
+      source: { id: null, name: "[Removed]" },
+      author: null,
+      title: "[Removed]",
+      description: "[Removed]",
+      url: "https://removed.com",
+      urlToImage: null,
+      publishedAt: "2024-12-02T20:21:25Z",
+      content: "[Removed]",
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title: "'I'm the UK's number one Trump impersonator'",
+      description:
+        "Donald Trump impressionist Mike Osman is inspired to keep performing after the US election.",
+      url: "https://www.bbc.com/news/articles/c5yxzrlge59o",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/8af2/live/11d0eb20-a8eb-11ef-bf4f-33b3bf22f30d.jpg",
+      publishedAt: "2024-11-24T07:30:35Z",
+      content:
+        "A Donald Trump impressionist has said his phone did not stop ringing after the result of the 2024 US presidential election.\r\nMike Osman, a comedian from Southampton, was almost ready to retire his bl… [+1666 chars]",
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title: "No plans to join Bluesky yet, Keir Starmer says",
+      description:
+        "The social media platform has been rapidly growing in popularity since the US presidential election.",
+      url: "https://www.bbc.com/news/articles/cwy12ny01e1o",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/2fc9/live/3b72feb0-a6dd-11ef-8f81-c98606b683e8.jpg",
+      publishedAt: "2024-11-20T02:03:41Z",
+      content:
+        'Sir Keir Starmer has said he has "no plans" to join the Bluesky social media platform.\r\nThere has been a rising wave of popularity for the app - an alternative to Elon Musk\'s X - in recent weeks.\r\nBl… [+1682 chars]',
+    },
+    {
+      source: { id: null, name: "[Removed]" },
+      author: null,
+      title: "[Removed]",
+      description: "[Removed]",
+      url: "https://removed.com",
+      urlToImage: null,
+      publishedAt: "2024-12-03T13:00:42Z",
+      content: "[Removed]",
+    },
+    {
+      source: { id: null, name: "[Removed]" },
+      author: null,
+      title: "[Removed]",
+      description: "[Removed]",
+      url: "https://removed.com",
+      urlToImage: null,
+      publishedAt: "2024-12-06T17:27:48Z",
+      content: "[Removed]",
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title: "Harris apologises for walking away from care worker",
+      description:
+        'The Fine Gael leader says "the buck stops with me" after tetchy filmed encounter ahead of the election.',
+      url: "https://www.bbc.com/news/articles/crr9vvnd4x1o",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/af55/live/fa007b70-aa3c-11ef-b439-f3906715f55d.jpg",
+      publishedAt: "2024-11-24T13:53:44Z",
+      content:
+        "Simon Harris said he was annoyed with himself over how he dealt with the situation\r\nTaoiseach (Irish prime minister) Simon Harris has apologised to a disability care worker he walked away from when s… [+3640 chars]",
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title: "Xi Jinping not expected to attend Trump inauguration",
+      description:
+        "China's ambassador is expected to attend and other officials may join him, two people told the BBC's US partner CBS.",
+      url: "https://www.bbc.com/news/articles/cgm9jvek93po",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/b073/live/1a8dcbe0-b974-11ef-a76e-4d65d4c6203f.jpg",
+      publishedAt: "2024-12-13T19:51:38Z",
+      content:
+        "Chinese President Xi Jinping is unlikely to attend the US presidential inauguration in January despite an invitation from Donald Trump, according to two people familiar with the planning.\r\nChina's am… [+2218 chars]",
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title:
+        "Trump and Xi Jinping’s ‘rollercoaster’ relationship has soured - can they rebuild it?",
+      description:
+        "What their dynamic means for the future of US-China relations",
+      url: "https://www.bbc.com/news/articles/cdxvdqg8214o",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/5847/live/45f66150-a5d0-11ef-a4fe-a3e9a6c5d640.png",
+      publishedAt: "2024-11-19T03:35:01Z",
+      content:
+        "Donald Trump once declared that he and Chinese President Xi Jinping love each other\r\nIn a sports park next to the red walls and glossy blue tiles which surround Beijings Temple of Heaven, a group of … [+12915 chars]",
+    },
+    {
+      source: { id: "business-insider", name: "Business Insider" },
+      author: "Sinéad Baker",
+      title: "Trump's victory has already prompted a realignment on Ukraine",
+      description:
+        "Donald Trump's election victory is impacting Ukraine even before he takes office. The US is rushing aid, and talks around peace deals are ramping up.",
+      url: "https://www.businessinsider.com/trump-president-victory-already-realigning-russia-ukraine-war-putin-zelenskyy-2024-11",
+      urlToImage:
+        "https://i.insider.com/673b2ce8ede4eeae39281127?width=1200&format=jpeg",
+      publishedAt: "2024-11-18T12:19:42Z",
+      content:
+        "Servicemen of the 24th Mechanised Brigade in Ukraine's Donetsk region in November 2024.Oleg Petrasiuk/Ukrainian 24th Mechanised Brigade via AP\r\n<ul><li>Donald Trump's election victory is impacting Uk… [+5675 chars]",
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title:
+        "Trump vows to end birthright citizenship and pardon US Capitol rioters",
+      description:
+        "The president-elect tells NBC he will make a number of decisions quickly after he takes office again.",
+      url: "https://www.bbc.com/news/articles/cj30er1d6mxo",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/105c/live/5dff1b10-b5c1-11ef-aee0-8f944e833e8d.jpg",
+      publishedAt: "2024-12-09T00:18:11Z",
+      content:
+        "Trump begins his second presidential term in January and has told NBC he will issue a range of new executive orders\r\nPresident-elect Donald Trump has said he will look at pardons for those involved i… [+4655 chars]",
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title: "Four things the latest inflation figures tell us",
+      description:
+        "The inflation rate has risen, putting pressure on people's finances and casting doubt on interest rate cuts.",
+      url: "https://www.bbc.com/news/articles/ce8yp69n6z3o",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/cf69/live/77a478e0-a729-11ef-b084-fbb99ebdbb21.jpg",
+      publishedAt: "2024-11-20T13:03:52Z",
+      content:
+        "Your money is being stretched as the pace of price rises has risen.\r\nThe inflation rate, which charts the rising cost of living, increased to 2.3% in October.\r\nPrices aren't soaring at the rate seen … [+4506 chars]",
+    },
+    {
+      source: { id: "wired", name: "Wired" },
+      author: "Jason Parham",
+      title: "The Red Elephant in the Room at AfroTech",
+      description:
+        "At the annual tech conference for Black professionals, all eyes were on AI—but many also braced for the impact of a Trump administration that has promised to kill DEI.",
+      url: "https://www.wired.com/story/afrotech-2024-red-elephant-in-the-room/",
+      urlToImage:
+        "https://media.wired.com/photos/673d1aef902059d462488522/191:100/w_1280,c_limit/Afrotech-Red-Elephant-in-the-Room-Culture-2184363872.jpg",
+      publishedAt: "2024-11-21T17:34:11Z",
+      content:
+        "Inside a nondescript brick warehouse on Preston Street in Houston, a crowd gathered with their hopes cast on the future. Assembled among the 60-person flock were Byron Spruell, president of league op… [+3814 chars]",
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title: "Elon Musk is a 'political puppet master', says Clegg",
+      description:
+        "Former deputy PM turned top Meta boss Nick Clegg sits down for a wide ranging interview with Nick Robinson.",
+      url: "https://www.bbc.com/news/articles/cvg7dyn3m0lo",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/7187/live/bb7c8830-b324-11ef-9eea-4f995c70052c.png",
+      publishedAt: "2024-12-06T06:00:12Z",
+      content:
+        'Billionaire and X owner Elon Musk is becoming a "political puppet master" in the US, former deputy prime minister Nick Clegg has said.\r\nClegg, who is now Mark Zuckerberg\'s second-in-command at Meta, … [+3221 chars]',
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title: "Brazil first lady uses expletive against Elon Musk at G20 event",
+      description:
+        "Janja Lula da Silva joked that she was not afraid of the owner of social media platform X.",
+      url: "https://www.bbc.com/news/articles/cwy1693xwlzo",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/fd0e/live/43abf3a0-a4f5-11ef-9886-f1f9e5342481.jpg",
+      publishedAt: "2024-11-17T16:03:32Z",
+      content:
+        "Brazil's first lady has sworn at billionaire Elon Musk at an event ahead of the G20 summit in Rio de Janeiro.\r\nWhile advocating for tougher social media regulation on a panel about disinformation, Ja… [+1488 chars]",
+    },
+    {
+      source: { id: "wired", name: "Wired" },
+      author: "Adrienne So",
+      title: "Peloton Strength+ Weight Lifting App: Details, Download, Price",
+      description:
+        "I’m sick of it all and I need to be able to deadlift a body. Thankfully, Peloton has a brand-new app geared toward weights and strength training.",
+      url: "https://www.wired.com/story/peloton-strength-plus-weightlifting-app/",
+      urlToImage:
+        "https://media.wired.com/photos/674f7f65ea6c0af41b136c34/191:100/w_1280,c_limit/peleton-homegym-gear.jpg",
+      publishedAt: "2024-12-04T15:00:00Z",
+      content:
+        "A few weeks ago, my friends and Iwomen ranging from our mid-thirties to mid-fortiesall started weight lifting.\r\nThis is for several reasons. In general, strength training and functional fitness are m… [+2013 chars]",
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title: "Time names Donald Trump person of the year for second time",
+      description:
+        "The Republican president-elect also won the award in 2016 when he first won the presidency.",
+      url: "https://www.bbc.com/news/articles/c36e71wg7z9o",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/38bf/live/21a24820-b807-11ef-85a9-bf8b1bb4e9e8.jpg",
+      publishedAt: "2024-12-12T13:15:31Z",
+      content:
+        'Time Magazine has named Donald Trump as their person of the year for the second time. \r\n"For marshaling a comeback of historic proportions, for driving a once-in-a- generation political realignment, … [+2590 chars]',
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title: "New Mauritius PM has reservations about UK's Chagos deal",
+      description:
+        "Navinchandra Ramgoolam, in power for a fortnight, spoke after meeting British envoy Jonathan Powell.",
+      url: "https://www.bbc.com/news/articles/crr9y0rz15vo",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/ce72/live/66d5dfa0-ac23-11ef-bdf5-b7cb2fa86e10.jpg",
+      publishedAt: "2024-11-26T19:04:56Z",
+      content:
+        "The new prime minister of Mauritius has said he has reservations about a deal struck by his predecessor with the UK government over the Chagos Islands.\r\nAgreed last month, it said the UK would give u… [+3325 chars]",
+    },
+    {
+      source: { id: "business-insider", name: "Business Insider" },
+      author: "Katherine Tangalakis-Lippert",
+      title:
+        "Bluesky's 'acceleration is tremendous' as users flock to the new 'old Twitter'",
+      description:
+        'Bluesky saw a surge in new users following the election, with many praising the feeling on the social media site as being similar to "old Twitter."',
+      url: "https://www.businessinsider.com/why-people-going-bluesky-old-twitter-2024-11",
+      urlToImage:
+        "https://i.insider.com/673642db192f525898589766?width=1200&format=jpeg",
+      publishedAt: "2024-11-19T01:28:06Z",
+      content:
+        'Many new Bluesky users praise the site\'s similarities to "old Twitter."Bluesky; App Store\r\n<ul><li>Since the election, Bluesky has acquired millions of new users and surpassed Threads on app stores.<… [+5884 chars]',
+    },
+    {
+      source: { id: "business-insider", name: "Business Insider" },
+      author: "htan@insider.com (Huileng Tan)",
+      title: "Trump's saber rattling about tariffs is unsettling China",
+      description:
+        "Chinese firms are on edge and so are some of their US partners. Markets are relatively muted, though the yuan is down.",
+      url: "https://www.businessinsider.com/how-china-is-reacting-to-trump-tariffs-markets-manufacturing-2024-11",
+      urlToImage:
+        "https://i.insider.com/674975cfede4eeae392a49c3?width=1200&format=jpeg",
+      publishedAt: "2024-11-29T08:00:27Z",
+      content:
+        "Post-election, Donald Trump is amplifying his threats to slap higher tariffs on imports into the US and China is unsettled.\r\nOn Monday, the president-elect took aim at Canada, Mexico, and China on hi… [+4404 chars]",
+    },
+    {
+      source: { id: null, name: "BBC News" },
+      author: null,
+      title: "'We couldn’t get a mortgage so live in a mobile home'",
+      description:
+        "A father says his family's mortgage offer was withdrawn when his partner became pregnant.",
+      url: "https://www.bbc.com/news/articles/c0mz3zj1kevo",
+      urlToImage:
+        "https://ichef.bbci.co.uk/news/1024/branded_news/3d67/live/1f274dd0-aaa5-11ef-bdf5-b7cb2fa86e10.jpg",
+      publishedAt: "2024-11-26T07:07:41Z",
+      content:
+        "Eoin McGovern and his son outside their mobile home\r\nA man living in a mobile home with his young family says owning their own home in the Republic of Ireland feels impossible.\r\nEoin McGovern from Co… [+4651 chars]",
+    },
+  ],
+};
