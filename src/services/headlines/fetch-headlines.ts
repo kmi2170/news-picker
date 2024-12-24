46e6827aa0a03898d90c0e9aefd050c71deff012
kmi2170 (Kemmei Hamada) <sageglorieux2170@hotmail.com>
2024-12-23 21:36:39 -0800

    add category param to fetch headlines

--- src/services/headlines/fetch-headlines.ts
+++ src/services/headlines/fetch-headlines.ts
@@ -1,19 +1,25 @@
 "use server";
 
-import { HeadlinesReturnType } from "../../api/types";
+import { HeadlineCategoryQuery, HeadlinesReturnType } from "../../api/types";
 
 const baseUrl = "https://newsapi.org/v2/top-headlines";
 const apiKey = process.env.NEWS_API_KEY as string;
 
-export const fetchHeadlines = async (): Promise<
-  HeadlinesReturnType | Error
-> => {
-  const searchParamsObj = {
-    // country: 'us',
-    category: "business",
+export const fetchHeadlines = async (
+  category: HeadlineCategoryQuery
+): Promise<HeadlinesReturnType | Error> => {
+  let searchParamsObj: Record<string, string> = {
+    country: "us",
     apiKey,
     pageSize: "100",
   };
+
+  if (category && category !== "all") {
+    searchParamsObj = {
+      category,
+      ...searchParamsObj,
+    };
+  }
 
   const searchParams = new URLSearchParams(searchParamsObj);
   const url = `${baseUrl}?${searchParams.toString()}`;
@@ -24,6 +30,7 @@ export const fetchHeadlines = async (): Promise<
       throw Error("Failed to fetch Headlines");
     }
     const data = await response.json();
+
     return data;
   } catch (error) {
     throw Error(error);
